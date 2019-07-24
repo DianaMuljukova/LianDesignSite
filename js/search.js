475195e9f82be2cd4628835ec7527db0a461268f
DianaMuljukova <pavlenkodr@yandex.ru>
2019-07-24 20:41:26 +0300

    adaptation mobile tablet

--- js/search.js
+++ js/search.js
@@ -8,13 +8,36 @@ var exit = document.querySelector('.fa-times')
 searhIcon.onclick = function (e) {
   searchBlock.style.display = 'flex';
   searchBlock.style.alignItems = 'flex-end';
-  searchBlock.style.position = 'absolute';
-  searchBlock.style.top = '-100%';
-  searchBlock.style.left = '100%';
+
   searhIcon.style.display = 'none';
 };
 
 exit.onclick = function (e) {
   searchBlock.style.display = 'none';
   searhIcon.style.display = 'block';
+}
+
+//меню
+
+var nav = document.querySelector('nav');
+var menuIcon = document.querySelector('.menu__icon');
+
+if (document.documentElement.clientWidth < 480){
+  nav.style.display = 'none';
+  menuIcon.style.display = 'block';
+} else {
+  nav.style.display = 'block';
+  menuIcon.style.display = 'none';
+}
+
+var isPressed = false;
+
+menuIcon.onclick = function (e) {
+if (isPressed == false){
+  nav.style.display = 'block';
+  isPressed = true;
+} else {
+  nav.style.display = 'none';
+  isPressed = false;
+}
 }
